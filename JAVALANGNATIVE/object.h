abe6fdacfa1c479bf26466948d9cf140271a21e9
ikrix32 <cristianserrban@yahoo.com>
2015-02-17 03:19:42 +0200

    java bytecode as byte arrays

--- JAVALANGNATIVE/object.h
+++ JAVALANGNATIVE/object.h
@@ -2,6 +2,8 @@
  * FHW-Berlin, Fachbereich Berufsakademie, Fachrichtung Informatik
  * See the file "license.terms" for information on usage and redistribution of this file.
  */
+#ifndef _KJVM_OBJECT_
+#define _KJVM_OBJECT_
 // Object
 #ifndef TINYBAJOS_MULTITASKING
 char nativeWait(void);
@@ -11,3 +13,4 @@ char nativeWait(void);
 char waitTime(void);
 #endif
 char getDataAddress(void);
+#endif
--- PLATFORMS/LINUX/platform.c
+++ PLATFORMS/LINUX/platform.c
@@ -11,13 +11,122 @@
 #include "classfile.h"
 #include "heap.h"
 #include "platform.h"
-/*
-const u1 java_lang_Object[] =
-#include "JVM/bootstrap_classes/Object.h"
 
-const u1 javaobject[] =
-#include "bootstrap_classes/Object.h"
-*/
+const u1 java_lang_ObjectBin[] =
+#include "java_lang_Object.h"
+
+const u1 platformBin[] =
+#include "java_PlatForm.h"
+
+const u1 java_lang_BooleanBin[] =
+#include "java_lang_Boolean.h"
+
+const u1 java_lang_ByteBin[] =
+#include "java_lang_Byte.h"
+
+const u1 java_lang_CharacterBin[] =
+#include "java_lang_Character.h"
+
+const u1 java_lang_ShortBin[] =
+#include "java_lang_Short.h"
+
+const u1 java_lang_IntegerBin[] =
+#include "java_lang_Integer.h"
+
+const u1 java_lang_FloatBin[] =
+#include "java_lang_Float.h"
+
+const u1 java_lang_CharSequenceBin[] =
+#include "java_lang_CharSequence.h"
+
+const u1 java_lang_StringBin[] =
+#include "java_lang_String.h"
+
+const u1 java_lang_StringBufferBin[] =
+#include "java_lang_StringBuffer.h"
+
+const u1 java_lang_StringBuilderBin[] =
+#include "java_lang_StringBuilder.h"
+
+const u1 java_lang_ErrorBin[] =
+#include "java_lang_Error.h"
+
+const u1 java_lang_ThrowableBin[] =
+#include "java_lang_Throwable.h"
+
+const u1 java_lang_ExceptionBin[] =
+#include "java_lang_Exception.h"
+
+const u1 java_lang_NullPointerExceptionBin[] =
+#include "java_lang_NullPointerException.h"
+
+const u1 java_lang_ArithmeticExceptionBin[] =
+#include "java_lang_ArithmeticException.h"
+
+const u1 java_lang_ArrayIndexOutOfBoundsExceptionBin[] =
+#include "java_lang_ArrayIndexOutOfBoundsException.h"
+
+const u1 java_lang_RuntimeExceptionBin[] =
+#include "java_lang_RuntimeException.h"
+
+const u1 java_lang_IllegalArgumentExceptionBin[] =
+#include "java_lang_IllegalArgumentException.h"
+
+const u1 java_lang_ClassCastExceptionBin[] =
+#include "java_lang_ClassCastException.h"
+
+const u1 java_lang_InterruptedExceptionBin[] =
+#include "java_lang_InterruptedException.h"
+
+const u1 java_lang_RuntimeBin[] =
+#include "java_lang_Runtime.h"
+
+const u1 java_lang_ThreadBin[] =
+#include "java_lang_Thread.h"
+
+const u1 java_lang_mathBin[] =
+#include "java_lang_Math.h"
+
+const u1 java_io_InStreamBin[] =
+#include "java_io_InStream.h"
+
+const u1 java_io_OutStreamBin[] =
+#include "java_io_OutStream.h"
+
+const u1 java_lang_SystemBin[] =
+#include "java_lang_System.h"
+
+const u1* bootStrapBinaries[] =
+{   java_lang_ObjectBin,
+    platformBin,
+    java_lang_BooleanBin,
+    java_lang_ByteBin,
+    java_lang_CharacterBin,
+    java_lang_ShortBin,
+    java_lang_IntegerBin,
+    java_lang_FloatBin,
+    java_lang_CharSequenceBin,
+    java_lang_StringBin,
+    java_lang_StringBufferBin,
+    java_lang_StringBuilderBin,
+    java_lang_ErrorBin,
+    java_lang_ThrowableBin,
+    java_lang_ExceptionBin,
+    java_lang_NullPointerExceptionBin,
+    java_lang_ArithmeticExceptionBin,
+    java_lang_ArrayIndexOutOfBoundsExceptionBin,
+    java_lang_RuntimeExceptionBin,
+    java_lang_IllegalArgumentExceptionBin,
+    java_lang_ClassCastExceptionBin,
+    java_lang_InterruptedExceptionBin,
+    java_lang_RuntimeBin,
+    java_lang_ThreadBin,
+    java_lang_mathBin,
+    java_io_InStreamBin,
+    java_io_OutStreamBin,
+    java_lang_SystemBin
+};
+
 void initHW()
 {
 }
@@ -28,7 +137,7 @@ void initHW()
 void initVM(int argc, char* argv[])               /* read, analyze classfiles and fill structures*/
 {
     u4 length;
-
+    printf("Size of java obj %lu\n",sizeof(java_lang_ObjectBin));
 #if (AVR32LINUX||LINUX)
     classFileBase=(char*)malloc((size_t) MAXBYTECODE);
     if (classFileBase==NULL)
