7469d41be439b939cbc85068bb8859216e42060f
ikrix32 <cristianserrban@yahoo.com>
2015-02-26 00:44:24 +0200

    no message

--- PLATFORMS/LINUX/platform.h
+++ PLATFORMS/LINUX/platform.h
@@ -1,4 +1,4 @@
-void initHW();
-void loadBootstrapClasses();
+void initHW(void);
+void loadBootstrapClasses(void);
 u2 readClassFile(char* fileName, char* addr);
 u2 readClassBin(const u1* bin,const u4 binSize, char* addr);
